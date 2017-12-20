5b2e20ecf185005d42c70be0e37b41992ed8f2e4
venelrene <venelrene@gmail.com>
2017-12-20 07:27:14 -0500

    Initial commit

---
+++ assigOne.py
@@ -0,0 +1,11 @@
+# 1.Write a sample Python code to delete an element in a list?
+# 2. Write a sample Python code to return a value in a tuple?
+
+aList = ["Write", "sample", "Python", "code"]
+aList.remove('Python')
+
+# The differences between tuples and lists are, the tuples cannot be changed
+# unlike lists and tuples use parentheses, whereas lists use square brackets.
+
+tup = ('This', 'is', 'a', 'tuples', 'example', 1997, 2000)
+print "tup[0] is:", tup[0]
---
+++ assigTwo.py
@@ -0,0 +1,4 @@
+#1.Write a sample Python code to write a file?
+#2. Write a sample Python code to open  database?
+
+path = c:New Folder
