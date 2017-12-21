6f39fb2ebdb02600c1b8912104f115683c62cb08
venelrene <venelrene@gmail.com>
2017-12-21 08:50:10 -0500

    Assignment 1-2 completed

--- assigOne.py
+++ assigOne.py
@@ -7,5 +7,10 @@ aList.remove('Python')
 # The differences between tuples and lists are, the tuples cannot be changed
 # unlike lists and tuples use parentheses, whereas lists use square brackets.
 
+
+
+# The main differences between lists and tuples are:
+# Lists are enclosed in brackets ( [ ] ) and their elements and size can be changed,
+# while tuples are enclosed in parentheses ( ( ) ) and cannot be updated. Tuples can be thought
 tup = ('This', 'is', 'a', 'tuples', 'example', 1997, 2000)
 print "tup[0] is:", tup[0]
--- assigTwo.py
+++ assigTwo.py
@@ -1,4 +1,14 @@
 #1.Write a sample Python code to write a file?
-#2. Write a sample Python code to open  database?
 
-path = c:New Folder
+file_object = open("new_file_name.txt", "wb+")
+file_object.write("Hello World!\nJust created a sample Python code to write a file")
+file_object.close()
+
+
+print "Name of the file: ", file_object.name
+print "Closed or not : ", file_object.closed
+print "Opening mode : ", file_object.mode
+print "Softspace flag : ", file_object.softspace
+
+
+#2. Write a sample Python code to open  database?
