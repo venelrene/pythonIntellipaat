f25d6359ce5c7be064ef255eba3819ddff5d3fbd
venelrene <venelrene@gmail.com>
2017-12-23 21:39:24 -0500

    5/10 done

--- assigFour.py
+++ assigFour.py
@@ -1,26 +1,37 @@
 # 1. Write a program which prints hello world
 print("Hello World")
+
 # 2. Write a program which should use following operators
-# a. Addition
-# b. Subtraction
-# c. Multiplication
-# d. Division
-a = 2
-b = 3
-c = a + b
+    # a. Addition # b. Subtraction # c. Multiplication # d. Division
+a = 2; b = 3; c = a + b
 d = (a + b) % ((c * b) - a)
 print d
+
 # 4. Write a program using while loop
 count = 0
 while (count < 3):
     count += 1
     print "Number", count, "is less than the next number"
+
 # 5. Write a program using if else statement
+num =
+if num > 2:
+    print "Greater than 2"
+elif num:
+    print "Got a true statement"
+    print num
+else:
+    print "statement is false"
+
+
 # 6. Perform following operations on List:
-# a. Concatenation
-# b. Replication
-# c. Updation
-# d. Deletion
+    # a. Concatenation
+    # b. Replication
+    # c. Updation
+    # d. Deletion
+
+
+
 # 7. Write a program to print the current month calendar
 # 8. Write a program of file handling using following methods:
 # a. rename()
